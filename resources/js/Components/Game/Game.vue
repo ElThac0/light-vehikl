fd500b3ff7209432d60e9075c0aea76cc4ec920e
vehikl-oro <oro@vehikl.com>
2024-08-30 17:15:50 -0400

    Getting started

---
+++ resources/js/Components/Game/Game.vue
@@ -0,0 +1,25 @@
+<script setup>
+import Tile from './Tile.vue'
+import {onMounted, ref} from "vue";
+
+const arenaSize = 25;
+
+const board = new Array(arenaSize).fill(new Array(arenaSize).fill({}, 0, arenaSize), 0, arenaSize);
+
+</script>
+
+<template>
+  <div id="board" :style="{ 'grid-template-columns': '1fr '.repeat(arenaSize), 'grid-template-rows': '1fr '.repeat(arenaSize) }">
+    <template v-for="row in board">
+      <Tile v-for="tile in row" />
+    </template>
+  </div>
+</template>
+
+<style>
+#board {
+  max-width: 75vh;
+  margin: auto;
+  display: grid;
+}
+</style>
---
+++ resources/js/Components/Game/Tile.vue
@@ -0,0 +1,19 @@
+<script setup>
+
+defineProps({
+  x: Number,
+  y: Number,
+});
+
+</script>
+
+<template>
+  <div class="tile"></div>
+</template>
+
+<style>
+.tile {
+  aspect-ratio: 1;
+  border: 1px solid navy;
+}
+</style>
