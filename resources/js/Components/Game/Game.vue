b6434d61bfd3777b2bca1b7507a295d1756db0e0
vehikl-oro <oro@vehikl.com>
2024-09-03 16:54:05 -0400

    Moving player

--- resources/js/Components/Game/Game.vue
+++ resources/js/Components/Game/Game.vue
@@ -1,17 +1,34 @@
 <script setup>
 import Tile from './Tile.vue'
-import {onMounted, ref} from "vue";
+import {onMounted, ref, watch} from "vue";
 
-const arenaSize = 25;
+window.Echo.listen('game', 'move', () => {
+
+})
+
+const arenaSize = 50;
+
+const player = ref({x: 0, y: 0});
 
 const board = new Array(arenaSize).fill(new Array(arenaSize).fill({}, 0, arenaSize), 0, arenaSize);
+
+const handleKeyPress = (e) => {
+  switch (e.key) {
+    case 's':
+      player.value.x = player.value.x + 1;
+  }
+}
+
+onMounted(() => {
+  window.addEventListener('keydown', handleKeyPress);
+});
 
 </script>
 
 <template>
   <div id="board" :style="{ 'grid-template-columns': '1fr '.repeat(arenaSize), 'grid-template-rows': '1fr '.repeat(arenaSize) }">
-    <template v-for="row in board">
-      <Tile v-for="tile in row" />
+    <template v-for="(row, rowIdx) in board">
+      <Tile v-for="(tile, tileIdx) in row" :player="player" :x="rowIdx" :y="tileIdx"/>
     </template>
   </div>
 </template>
--- resources/js/Components/Game/Tile.vue
+++ resources/js/Components/Game/Tile.vue
@@ -1,19 +1,35 @@
 <script setup>
 
-defineProps({
+import {computed, reactive} from "vue";
+
+const props = defineProps({
+  player: {x: Number, y: Number},
   x: Number,
   y: Number,
+});
+
+const isPlayerHere = computed(() => {
+  return props.x === props.player.x && props.y === props.player.y;
 });
 
 </script>
 
 <template>
-  <div class="tile"></div>
+  <div
+    :class="{
+      tile: true,
+      player1: isPlayerHere,
+    }"
+  ></div>
 </template>
 
 <style>
 .tile {
   aspect-ratio: 1;
   border: 1px solid navy;
+}
+
+.player1 {
+  background-color: lime;
 }
 </style>
