d2cb86e6b8db1f4ef6fbc6041b173ed192a7053c
vehikl-oro <oro@vehikl.com>
2024-11-12 17:06:02 -0500

    Update player colors

--- resources/js/Components/Game/Tile.vue
+++ resources/js/Components/Game/Tile.vue
@@ -6,7 +6,26 @@ const props = defineProps({
   contents: Number,
 });
 
-const occupied = computed(() => props.contents !== 0);
+const player = computed(() => {
+  console.log(props.contents);
+    switch(props.contents)
+    {
+      case 2:
+      case 3:
+        return 'player-1';
+      case 4:
+      case 5:
+        return 'player-2';
+      case 6:
+      case 7:
+        return 'player-3';
+      case 8:
+      case 9:
+        return 'player-4';
+      default:
+        return 'empty';
+    }
+});
 
 </script>
 
@@ -14,7 +33,7 @@ const occupied = computed(() => props.contents !== 0);
   <div
     :class="{
       tile: true,
-      occupied,
+      [player]: true,
     }"
   ></div>
 </template>
@@ -23,6 +42,22 @@ const occupied = computed(() => props.contents !== 0);
 .tile {
   aspect-ratio: 1;
   border: 1px solid navy;
+}
+
+.player-1 {
+  background-color: lime;
+}
+
+.player-2 {
+  background-color: red;
+}
+
+.player-3 {
+  background-color: #2563eb;
+}
+
+.player-4 {
+  background-color: orange;
 }
 
 .occupied {
